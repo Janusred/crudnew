3dcec9004210c69b4754d8916e16a8666e546501
janusred <horaciok300@hotmail.com>
2021-03-08 02:16:32 -0600

    Vistas controladores, fallas3

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,16 +1,24 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\user;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index(){
-
+       $users = User::latest()->get();
+       return view('users.index',[
+           'users' => $users
+       ]);
     }
-    public function user(){
-        
+    public function user(Request $request){
+         User::created([
+             'name'=> $request->name,
+             'email'=> $request->email,
+             'password'=> $request->password,
+         ]);
+        return back();
     }
     public function destroy(){
         
